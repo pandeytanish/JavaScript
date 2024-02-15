6e3255eb73b48f336997c49ba11953d63a95815f
Tanish Pandey <tanishpandey14@gmail.com>
2024-02-15 19:51:10 +0530

    remove node using js

--- DOM/DOm part 2/script.js
+++ DOM/DOm part 2/script.js
@@ -1,8 +1,16 @@
-let newBtn = document.createElement("button");
-newBtn.innerText = "Click ME" ;
+// let newBtn = document.createElement("button");
+// newBtn.innerText = "Click ME" ;
 
-let div = document.querySelector("div");
-// div.append(newBtn);
-// div.prepend(newBtn);
-// div.before(newBtn);
-// div.after(newBtn);
+// let div = document.querySelector("div");
+// // div.append(newBtn);
+// // div.prepend(newBtn);
+// // div.before(newBtn);
+// // div.after(newBtn);
+
+let newHeading = document.createElement("h1");
+newHeading.innerText = "hello there";
+
+document.querySelector("body").append(newHeading);
+
+let para = document.querySelector("p");
+para.remove();
