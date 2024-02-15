269cceca88412c3258945560b54fd5d0050c4a7e
Tanish Pandey <tanishpandey14@gmail.com>
2024-02-15 19:43:33 +0530

    inserting element with js

---
+++ DOM/DOm part 2/script.js
@@ -0,0 +1,8 @@
+let newBtn = document.createElement("button");
+newBtn.innerText = "Click ME" ;
+
+let div = document.querySelector("div");
+// div.append(newBtn);
+// div.prepend(newBtn);
+// div.before(newBtn);
+// div.after(newBtn);
